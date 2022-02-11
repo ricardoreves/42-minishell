3d9e7185d964a01afb4ea8959d58c8791948c69c
ricardoreves <ricardo.reves@gmail.com>
2022-02-11 03:16:11 +0100

    fonctions builtin retiree  du fork enfant

--- srcs/command.c
+++ srcs/command.c
@@ -6,7 +6,7 @@
 /*   By: rpinto-r <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 18:35:14 by rpinto-r          #+#    #+#             */
-/*   Updated: 2022/02/10 00:14:01 by rpinto-r         ###   ########.fr       */
+/*   Updated: 2022/02/11 03:04:27 by rpinto-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,44 +47,49 @@ int exec_command(t_shell *shell)
 	char **args;
 	char *pathname;
 
-	cpid = fork();
-	if (cpid == 0)
+	args = eval_varenv_map(shell, ft_split(shell->cmdline, ' '));
+	// args = ft_split(shell->cmdline, ' ');
+	if (is_builtin(args))
 	{
-		args = eval_varenv_map(shell, ft_split(shell->cmdline, ' '));
-		printf("Child\n");
+		exec_builtin(shell, args);
+		free_array(args);
+		return (1);
+	}
 
+	cpid = fork();
+	if (cpid == -1)
+	{
+		perror("Error: fork() failed\n");
+		exit(1);
+	}
+	else if (cpid == 0)
+	{
+		printf("Child\n");
 		// printf("%d\n", access(args[0], R_OK));
-		if (is_builtin(args))
-			exec_builtin(shell, args);
-		else if (ft_strncmp(args[0], "/", 1) == 0)
+
+		if (ft_strncmp(args[0], "/", 1) == 0)
 			put_command_error(shell, args[0], "is a directory");
 		else if (find_command(shell, args[0], &pathname) == 0)
 			put_command_error(shell, args[0], "command not found");
 		else if (execve(pathname, args, shell->envs))
 		{
-			shell->error = errno;
+			// shell->error = errno;
 			put_command_error(shell, args[0], strerror(errno));
 		}
-			
-		// if (pathname)
-		//	free(pathname);
+
+		if (pathname)
+			free(pathname);
 		free_array(args);
+		free_array(shell->envs);
 		exit(0);
 	}
-	if (cpid > 0)
+	else
 	{
 		// wait(&wstatus);
 		waitpid(cpid, &wstatus, WUNTRACED);
 		printf("Parent\n");
-		if (ft_strncmp(shell->cmdline, "exit", 4) == 0)
-		{
-			free_array(shell->envs);
-			exit(0);
-		}
+		free_array(args);
 	}
-	if (cpid < 0)
-	{
-		perror("Error: fork() failed\n");
-	}
+
 	return (0);
 }
