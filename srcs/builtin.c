004450955853454b910bbb3386114eaf8e5cfe20
ricardoreves <ricardo.reves@gmail.com>
2022-02-28 00:15:55 +0100

    update prototypes

--- srcs/builtin.c
+++ srcs/builtin.c
@@ -6,47 +6,46 @@
 /*   By: rpinto-r <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 18:33:21 by rpinto-r          #+#    #+#             */
-/*   Updated: 2022/02/08 03:06:10 by rpinto-r         ###   ########.fr       */
+/*   Updated: 2022/02/28 00:06:12 by rpinto-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int is_builtin(char **args)
+int is_builtin(char *name)
 {
-    if (str_compare(args[0], "cd") == 0)
+    if (str_compare(name, "cd") == 0)
         return (1);
-    else if (str_compare(args[0], "echo") == 0)
+    else if (str_compare(name, "echo") == 0)
         return (1);
-    else if (str_compare(args[0], "env") == 0)
+    else if (str_compare(name, "env") == 0)
         return (1);
-    else if (str_compare(args[0], "exit") == 0)
+    else if (str_compare(name, "exit") == 0)
         return (1);
-    else if (str_compare(args[0], "export") == 0)
+    else if (str_compare(name, "export") == 0)
         return (1);
-    else if (str_compare(args[0], "pwd") == 0)
+    else if (str_compare(name, "pwd") == 0)
         return (1);
-    else if (str_compare(args[0], "unset") == 0)
+    else if (str_compare(name, "unset") == 0)
         return (1);
     else
         return (0);
 }
 
-void exec_builtin(t_shell *shell, char **args)
+void exec_builtin(t_shell *shell, t_cmd *cmd)
 {
-
-    if (str_compare(args[0], "cd") == 0)
-        exec_cd(shell, args);
-    else if (str_compare(args[0], "echo") == 0)
-        exec_echo(shell, args);
-    else if (str_compare(args[0], "env") == 0)
+    if (str_compare(cmd->name, "cd") == 0)
+        exec_cd(shell, cmd->args);
+    else if (str_compare(cmd->name, "echo") == 0)
+        exec_echo(shell, cmd->args);
+    else if (str_compare(cmd->name, "env") == 0)
         exec_env(shell);
-    else if (str_compare(args[0], "exit") == 0)
+    else if (str_compare(cmd->name, "exit") == 0)
         exec_exit(shell);
-    else if (str_compare(args[0], "export") == 0)
-        exec_export(shell, args);
-    else if (str_compare(args[0], "pwd") == 0)
+    else if (str_compare(cmd->name, "export") == 0)
+        exec_export(shell, cmd->args);
+    else if (str_compare(cmd->name, "pwd") == 0)
         exec_pwd(shell);
-    else if (str_compare(args[0], "unset") == 0)
-        exec_unset(shell, args);
+    else if (str_compare(cmd->name, "unset") == 0)
+        exec_unset(shell, cmd->args);
 }
