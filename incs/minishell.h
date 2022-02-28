cab324c3741cf769549b8fa3420e8ea8c2426b25
ricardoreves <ricardo.reves@gmail.com>
2022-02-28 04:21:45 +0100

    update prototypes

--- incs/minishell.h
+++ incs/minishell.h
@@ -6,7 +6,7 @@
 /*   By: rpinto-r <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 17:57:18 by rpinto-r          #+#    #+#             */
-/*   Updated: 2022/02/27 17:18:03 by rpinto-r         ###   ########.fr       */
+/*   Updated: 2022/02/28 03:25:24 by rpinto-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@
 #define WRITE_END 1
 #define CONFIGFILE ".config/parser/bash.conf"
 
+//#include <libft.h>
+//#include <libparser.h>
 #include "../libft/libft.h"
 #include "../parser/inc/libparser.h"
 #include <readline/readline.h>
@@ -39,10 +41,10 @@ typedef enum e_bash_token_id
 	id_blank = -1,
 	id_dbl_quotes = 1,
 	id_single_quotes,
-	id_out_write, // >
-	id_out_append,// >>
-	id_in_file,  // <
-	id_in_std,  // <<
+	id_out_write,
+	id_out_append,
+	id_in_file,
+	id_in_std,
 	id_pipe,
 	id_start_parenthesis,
 	id_end_parenthesis,
@@ -57,15 +59,15 @@ typedef enum e_bash_token_id
 	id_builtin_export,
 	id_builtin_pwd,
 	id_builtin_unset
-}	t_bash_token_id;
+} t_bash_token_id;
 
 typedef struct s_cmd
 {
-	char 			*name;
-	char			 **args;
-	int				redirect;
-	char 			*filename;
-	struct s_cmd	*next;
+	char *name;
+	char **args;
+	t_bash_token_id redirect;
+	char *filename;
+	struct s_cmd *next;
 } t_cmd;
 
 typedef struct s_shell
@@ -73,24 +75,15 @@ typedef struct s_shell
 	char *cmdline;
 	t_cmd *cmds;
 	char **envs;
-	char *configpath;
+	char *config;
 	int error;
 } t_shell;
-
-typedef enum e_redirect
-{
-	NO_REDIRECT,   // 0
-	INPUT_SINGLE,  // <
-	INPUT_DOUBLE,  // <<
-	OUTPUT_SINGLE, // >
-	OUTPUT_DOUBLE  // >>
-} t_redirect;
 
 void print_array(char *arr[]);
 void free_array(char *arr[]);
 int array_length(char *arr[]);
 char **clone_array(char *arr[]);
-void	push_array(char *str, char ***arr);
+void push_array(char *str, char ***arr);
 
 char *get_varenv_value(t_shell *shell, char *str, int *i);
 char *eval_varenv_str(t_shell *shell, char *str, int k);
@@ -103,7 +96,7 @@ int set_env(t_shell *shell, char *name, char *value);
 int unset_env(t_shell *shell, char *name);
 int init_envs(t_shell *shell, char *envp[]);
 
-void signal_handler(int signum);
+void handle_signals(int signum);
 void override_signals(void);
 
 char *str_joinsep(char *str1, char *str2, char *sep);
@@ -121,10 +114,15 @@ int exec_unset(t_shell *shell, char **args);
 
 void init_prompt(t_shell *shell);
 void init_asciiart(void);
-int is_builtin(char **args);
-void exec_builtin(t_shell *shell, char **args);
+int is_builtin(char *name);
+void exec_builtin(t_shell *shell, t_cmd *cmd);
 int exec_command(t_shell *shell);
 char *get_command_path(t_shell *shell, char *name);
+int handle_commands(t_shell *shell);
+
+int redirect_input(t_cmd *cmd);
+int redirect_output(t_cmd *cmd);
+void handle_redirect(t_cmd *cmd);
 
 // Not used
 typedef struct s_env
@@ -142,15 +140,17 @@ int init_envslst(t_shell *shell, char *envs[]);
 int add_envlst(t_env **envs, t_env *env);
 int set_envlst(t_env **envs, char *name, char *value);
 int unset_envlst(t_env **envs, char *name); // bug
-char	**parse(t_shell *shell, char *config);
-void	sanatize_quotes_token(t_token *token);
-void	exec_ident_token(t_token *token, t_shell *shell);
-void	set_token_builtin(t_token *token);
-char	**token_to_string(t_token *token_from, t_token *token_to);
-int		is_id_builtin(int id);
-int 	is_redirect(int id);
-int 	is_word(int id);
-void	prepare_cmd(t_token *token, t_cmd **cmds);
-void 	print_cmd(t_cmd *cmds);
-void	free_cmd(t_cmd **cmds);
+
+char **parse_command_line(t_shell *shell);
+void sanatize_quotes_token(t_token *token);
+void exec_ident_token(t_token *token, t_shell *shell);
+void set_token_builtin(t_token *token);
+char **token_to_string(t_token *token_from, t_token *token_to);
+int is_id_builtin(int id);
+int is_redirect(int id);
+int is_word(int id);
+void prepare_cmds(t_token *token, t_cmd **cmds);
+void print_cmds(t_cmd *cmds);
+void free_cmds(t_cmd *cmds);
+int count_cmds(t_cmd *cmds);
 #endif
