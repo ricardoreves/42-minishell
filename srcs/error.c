ce84d027cd23d807e63379f1428fbbddff41f385
ricardoreves <ricardo.reves@gmail.com>
2022-02-28 18:56:54 +0100

    print errno

--- srcs/error.c
+++ srcs/error.c
@@ -6,18 +6,19 @@
 /*   By: rpinto-r <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 18:41:02 by rpinto-r          #+#    #+#             */
-/*   Updated: 2022/02/08 01:51:25 by rpinto-r         ###   ########.fr       */
+/*   Updated: 2022/02/28 18:41:21 by rpinto-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int put_command_error(t_shell *shell, char *cmd, char *msg)
+int put_command_error(t_shell *shell, char *cmd, char *msg, int num)
 {
-	(void)shell;
+	shell->error = num;
 	ft_putstr_fd(cmd, 2);
 	ft_putstr_fd(": ", 2);
 	ft_putstr_fd(msg, 2);
 	ft_putendl_fd("", 2);
+	printf("errno: %d\n", num);
 	return (0);
 }
