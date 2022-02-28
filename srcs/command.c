7a2da5ebba5a91618e7bd2240f4bc57b39b1a3c9
ricardoreves <ricardo.reves@gmail.com>
2022-03-01 00:27:37 +0100

    move pipe functions to pipe.c

--- srcs/command.c
+++ srcs/command.c
@@ -6,46 +6,11 @@
 /*   By: rpinto-r <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 18:35:14 by rpinto-r          #+#    #+#             */
-/*   Updated: 2022/03/01 00:24:15 by rpinto-r         ###   ########.fr       */
+/*   Updated: 2022/03/01 00:26:31 by rpinto-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-void close_pipes(t_shell *shell)
-{
-	int i;
-
-	i = shell->num_pipes;
-	while (i--)
-	{
-		if (close(shell->pipes[i][0]) != 0 || close(shell->pipes[i][1]) != 0)
-			perror("Error: pipe close() failed");
-	}
-}
-
-int create_pipes(t_shell *shell)
-{
-	int i;
-
-	i = 0;
-	shell->num_pipes = shell->num_cmds - 1;
-	shell->pipes = (int **)ft_calloc(sizeof(int *), shell->num_pipes);
-	if (!shell->pipes)
-		return (0);
-	i = 0;
-	while (i < shell->num_pipes)
-	{
-		shell->pipes[i] = (int *)ft_calloc(sizeof(int), 2);
-		if (pipe(shell->pipes[i]) != 0)
-		{
-			perror("Error: pipe() failed");
-			return (0);
-		}
-		i++;
-	}
-	return (1);
-}
 
 void child_process_command(t_shell *shell, t_cmd *cmd, int num)
 {
