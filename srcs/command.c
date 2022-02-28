8c92a2d660152a5137d227b451956fa85fedd8ef
ricardoreves <ricardo.reves@gmail.com>
2022-02-28 22:40:44 +0100

    add support  command pipes

--- srcs/command.c
+++ srcs/command.c
@@ -6,11 +6,96 @@
 /*   By: rpinto-r <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 18:35:14 by rpinto-r          #+#    #+#             */
-/*   Updated: 2022/02/28 19:38:19 by rpinto-r         ###   ########.fr       */
+/*   Updated: 2022/02/28 22:35:41 by rpinto-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void close_pipes(t_shell *shell)
+{
+	int i;
+
+	i = shell->num_pipes;
+	while (i--)
+	{
+		if (close(shell->pipes[i][0]) != 0 || close(shell->pipes[i][1]) != 0)
+			perror("Error: pipe close() failed");
+	}
+}
+
+int create_pipes(t_shell *shell)
+{
+	int i;
+
+	i = 0;
+	shell->num_pipes = shell->num_cmds - 1;
+	shell->pipes = (int **)ft_calloc(sizeof(int *), shell->num_pipes);
+	if (!shell->pipes)
+		return (0);
+	i = 0;
+	while (i < shell->num_pipes)
+	{
+		shell->pipes[i] = (int *)ft_calloc(sizeof(int), 2);
+		if (pipe(shell->pipes[i]) != 0)
+		{
+			perror("Error: pipe() failed");
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+void child_process_command(t_shell *shell, t_cmd *cmd, int idx)
+{
+	pid_t cpid;
+	// int wstatus;
+
+	cpid = fork();
+	if (cpid < 0) // Error
+	{
+		perror("Error: fork() failed");
+	}
+	else if (cpid == 0) // Child
+	{
+		// input
+		if (idx > 0)
+		{
+			dup2(shell->pipes[idx - 1][0], 0);
+		}
+		else if (idx == 0) // first
+		{
+			handle_redirect(cmd);
+		}
+
+		// output
+		if (idx < shell->num_cmds - 1)
+		{
+			dup2(shell->pipes[idx][1], 1);
+		}
+		else if (idx == shell->num_cmds - 1) // last
+		{
+			handle_redirect(cmd);
+		}
+		close_pipes(shell);
+		if (access_command(get_env(shell, "PATH"), &cmd->name) == 0)
+		{
+			put_command_error(shell, cmd->name, "command not found", 127);
+		}
+		else if (execve(cmd->name, cmd->args, shell->envs))
+		{
+			put_command_error(shell, cmd->name, strerror(errno), errno);
+		}
+		free_shell(shell);
+		exit(0);
+	}
+	else // Parent
+	{
+		// waitpid(cpid, &wstatus, WCONTINUED);
+		printf("cpid %d = %d\n", idx, cpid);
+	}
+}
 
 /*
 is directory
@@ -64,21 +149,33 @@ int access_command(char *path, char **name)
 	return (0);
 }
 
-int handle_commands(t_shell *shell)
+void handle_commands(t_shell *shell)
 {
-	int count;
+	int i;
+	t_cmd *cmd;
+	int wstatus;
 
-	count = count_cmds(shell->cmds);
-	if (count == 1)
-	{   
-	    if (shell->cmds->name)
-		   exec_command(shell);
-	}
-	else if (count > 1)
+	if (shell->num_cmds == 1)
 	{
+		if (shell->cmds->name)
+			exec_command(shell);
 	}
-	printf("nb_cmds: %d\n", count_cmds(shell->cmds));
-	return 0;
+	else if (shell->num_cmds > 1)
+	{
+		// print_cmds(shell->cmds);
+		i = 0;
+		cmd = shell->cmds;
+		create_pipes(shell);
+		while (cmd)
+		{
+			child_process_command(shell, cmd, i);
+			cmd = cmd->next;
+			i++;
+		}
+		close_pipes(shell);
+		waitpid(-1, &wstatus, WCONTINUED);
+		free_cmds(shell->cmds);
+	}
 }
 
 int exec_command(t_shell *shell)
@@ -100,8 +197,6 @@ int exec_command(t_shell *shell)
 		else if (cpid == 0)
 		{
 			handle_redirect(shell->cmds);
-			// print_cmds(shell->cmds);
-			printf("%d\n", execve(shell->cmds->name, shell->cmds->args, shell->envs));
 			if (execve(shell->cmds->name, shell->cmds->args, shell->envs))
 			{
 				put_command_error(shell, shell->cmds->name, strerror(errno), errno);
@@ -109,7 +204,7 @@ int exec_command(t_shell *shell)
 			free_shell(shell);
 			exit(0);
 		}
-		waitpid(cpid, &wstatus, WUNTRACED);
+		waitpid(cpid, &wstatus, WCONTINUED);
 		free_cmds(shell->cmds);
 	}
 	return (0);
