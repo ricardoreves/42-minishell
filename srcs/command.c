f825cab78bcccf2107bfd34984a6f163e0d0d777
ricardoreves <ricardo.reves@gmail.com>
2022-03-01 23:21:07 +0100

    update code

--- srcs/command.c
+++ srcs/command.c
@@ -6,66 +6,11 @@
 /*   By: rpinto-r <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 18:35:14 by rpinto-r          #+#    #+#             */
-/*   Updated: 2022/03/01 19:50:26 by rpinto-r         ###   ########.fr       */
+/*   Updated: 2022/03/01 23:18:20 by rpinto-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-void create_pids(t_shell *shell)
-{
-	shell->pids = ft_calloc(sizeof(int), shell->num_cmds);
-}
-
-void child_process_command(t_shell *shell, t_cmd *cmd, int num)
-{
-	//pid_t cpid;
-	//int wstatus;
-
-	shell->pids[num] = fork();
-	if (shell->pids[num] < 0) // Error
-	{
-		perror("Error: fork() failed");
-	}
-	else if (shell->pids[num] == 0) // Child
-	{
-		// input
-		if (num > 0)
-		{
-			//printf("input -> %s pipe: [%d][0] std: 0\n", cmd->name, num - 1);
-			dup2(shell->pipes[num - 1][0], 0);
-		}
-		else if (num == 0) // first command
-		{
-			// handle_redirect(cmd);
-		}
-
-		// output
-		if (num < shell->num_cmds - 1)
-		{
-			//printf("output -> %s pipe: [%d][1] std: 1\n", cmd->name, num);
-			dup2(shell->pipes[num][1], 1);
-		}
-		else if (num == shell->num_cmds - 1) // last command
-		{
-			// handle_redirect(cmd);
-		}
-		close_pipes(shell);
-		if (access_command(get_env(shell, "PATH"), &cmd->name) == 0)
-		{
-			put_command_error(shell, cmd->name, "command not found", 127);
-		}
-		else if (execve(cmd->name, cmd->args, shell->envs))
-		{
-			put_command_error(shell, cmd->name, strerror(errno), errno);
-		}
-		free_shell(shell);
-		exit(0);
-	}
-	//wait(&wstatus);
-	// waitpid(cpid, &wstatus, WCONTINUED);
-	//printf("cpid %d = %d\n", num, cpid);
-}
 
 /*
 is directory
@@ -79,7 +24,7 @@ bash: ./l: No such file or directory
 bash: //kk: No such file or directory
  */
 
-int is_directory(char *path) // 126
+int is_directory_command(char *path) // 126
 {
 	int i;
 
@@ -91,6 +36,48 @@ int is_directory(char *path) // 126
 		i++;
 	}
 	return (1);
+}
+
+void process_command(t_shell *shell, t_cmd *cmd, int num)
+{
+	shell->pids[num] = fork();
+	if (shell->pids[num] < 0)
+		perror("Error: fork() failed");
+	else if (shell->pids[num] == 0) // Child
+	{
+		// input
+		if (num > 0)
+		{
+			// printf("input -> %s pipe: [%d][0] std: 0\n", cmd->name, num - 1);
+			dup2(shell->pipes[num - 1][0], 0);
+		}
+		else if (num == 0) // first command
+		{
+			// handle_redirect(cmd);
+		}
+
+		// output
+		if (num < shell->num_cmds - 1)
+		{
+			// printf("output -> %s pipe: [%d][1] std: 1\n", cmd->name, num);
+			dup2(shell->pipes[num][1], 1);
+		}
+		else if (num == shell->num_cmds - 1) // last command
+		{
+			// handle_redirect(cmd);
+		}
+		close_pipes(shell);
+		if (is_builtin_command(cmd->name))
+			exec_builtin_command(shell, cmd);
+		else if (is_directory_command(cmd->name))
+			put_command_error(shell, cmd->name, "is a directory", 126);
+		else if (access_command(get_env(shell, "PATH"), &cmd->name) == 0)
+			put_command_error(shell, cmd->name, "command not found", 127);
+		else if (execve(cmd->name, cmd->args, shell->envs))
+			put_command_error(shell, cmd->name, strerror(errno), errno);
+		free_shell(shell);
+		exit(0);
+	}
 }
 
 int access_command(char *path, char **name)
@@ -123,55 +110,49 @@ void handle_commands(t_shell *shell)
 {
 	int i;
 	t_cmd *cmd;
-	int wstatus;
 
-	if (shell->num_cmds == 1)
+	i = 0;
+	cmd = shell->cmds;
+	if (cmd->next == 0 && is_builtin_command(cmd->name))
+		exec_builtin_command(shell, cmd);
+	else if (is_directory_command(cmd->name))
+		put_command_error(shell, cmd->name, "is a directory", 126);
+	else if (access_command(get_env(shell, "PATH"), &cmd->name) == 0)
+		put_command_error(shell, cmd->name, "command not found", 127);
+	else
 	{
-		if (shell->cmds->name)
-			exec_single_command(shell);
-	}
-	else if (shell->num_cmds > 1)
-	{
-		// print_cmds(shell->cmds);
-		i = 0;
-		cmd = shell->cmds;
 		create_pids(shell);
 		create_pipes(shell);
 		while (cmd)
 		{
-			child_process_command(shell, cmd, i);
+			process_command(shell, cmd, i);
 			cmd = cmd->next;
 			i++;
 		}
 		close_pipes(shell);
-		i = 0;
-		while (i < shell->num_cmds)
-		{
-			//printf("num: %d pid: %d\n", i, shell->pids[i]);
-			waitpid(shell->pids[i], &wstatus, 0);
-			i++;
-		}
+		wait_pids(shell);
 		free_cmds(shell->cmds);
 	}
 }
 
+/* unused */
 int exec_single_command(t_shell *shell)
 {
 	int wstatus;
-	pid_t cpid;
+	pid_t pid;
 
 	if (is_builtin_command(shell->cmds->name))
 		exec_builtin_command(shell, shell->cmds);
-	else if (is_directory(shell->cmds->name))
+	else if (is_directory_command(shell->cmds->name))
 		put_command_error(shell, shell->cmds->name, "is a directory", 126);
 	else if (access_command(get_env(shell, "PATH"), &shell->cmds->name) == 0)
 		put_command_error(shell, shell->cmds->name, "command not found", 127);
 	else
 	{
-		cpid = fork();
-		if (cpid == -1)
+		pid = fork();
+		if (pid == -1)
 			perror("Error: fork() failed\n");
-		else if (cpid == 0)
+		else if (pid == 0)
 		{
 			handle_redirect(shell->cmds);
 			if (execve(shell->cmds->name, shell->cmds->args, shell->envs))
@@ -181,7 +162,7 @@ int exec_single_command(t_shell *shell)
 			free_shell(shell);
 			exit(0);
 		}
-		waitpid(cpid, &wstatus, WCONTINUED);
+		waitpid(pid, &wstatus, WCONTINUED);
 		free_cmds(shell->cmds);
 	}
 	return (0);
