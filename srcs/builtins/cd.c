8e759285de4e0ce9da216491e89a00f4305c008a
ricardoreves <ricardo.reves@gmail.com>
2022-03-02 03:14:17 +0100

    refactorize exec_cd

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: rpinto-r <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 21:28:31 by rpinto-r          #+#    #+#             */
-/*   Updated: 2022/02/28 16:57:31 by rpinto-r         ###   ########.fr       */
+/*   Updated: 2022/03/02 01:40:09 by rpinto-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,45 +24,32 @@ void update_pwd(t_shell *shell, char *name)
 
     ft_bzero(cwd, sizeof(cwd));
     if (getcwd(cwd, sizeof(cwd)))
-    {
-        //printf("UPDATE_PWD>>%s: %s\n", name, cwd);
         set_env(shell, ft_strdup(name), ft_strdup(cwd));
-    }
     else
         perror("Error: getcwd() failed");
 }
 
-int exec_cd(t_shell *shell, char *args[])
+int exec_cd(t_shell *shell, t_cmd *cmd)
 {
     int i;
     char **dirs;
 
-    // add_env(shell, ft_strdup("KKKK"), ft_strdup("0"));
     update_pwd(shell, "OLDPWD");
-
-    //add_env(shell, ft_strdup("MAR"), ft_strdup("555"));
-    //set_env(shell, ft_strdup("MAR"), ft_strdup("9999999"));
-    //printf("EXEC_CD>>%s\n", get_env(shell, "MAR"));
-
-    // print_array(shell->envs);
-
-    //
-    if (!args[1] || (args[1] && str_compare(args[1], "~") == 0))
+    if (!cmd->args[1] || (cmd->args[1] && str_compare(cmd->args[1], "~") == 0))
         change_dir(shell, get_env(shell, "HOME"));
-    else if (args[1] && str_compare(args[1], "-") == 0)
+    else if (cmd->args[1] && str_compare(cmd->args[1], "-") == 0)
         ft_putendl_fd(get_env(shell, "PWD"), 1);
-    else if (ft_strchr(args[1], '/') != 0)
+    else if (ft_strchr(cmd->args[1], '/') != 0)
     {
         i = -1;
-        dirs = ft_split(args[1], '/');
+        dirs = ft_split(cmd->args[1], '/');
         while (dirs[++i])
             change_dir(shell, dirs[i]);
+        free_array(dirs);
     }
     else
-        change_dir(shell, args[1]);
+        change_dir(shell, cmd->args[1]);
     update_pwd(shell, "PWD");
-    //printf("EXEC_CD>>%s\n", get_env(shell, "OLDPWD"));
-    //printf("EXEC_CD>>%s\n", get_env(shell, "PWD"));
     return (0);
 }
 
