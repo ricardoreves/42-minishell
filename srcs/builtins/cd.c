0a5ee55a5cfea4d9ead47bea04dcdf64f4a6d6f6
ricardoreves <ricardo.reves@gmail.com>
2022-03-06 04:26:21 +0100

    factorize: all functions

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -6,60 +6,73 @@
 /*   By: rpinto-r <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 21:28:31 by rpinto-r          #+#    #+#             */
-/*   Updated: 2022/03/05 21:59:42 by rpinto-r         ###   ########.fr       */
+/*   Updated: 2022/03/06 04:24:53 by rpinto-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void change_dir(t_shell *shell, char *path)
-{
-    if (chdir(path) != 0)
-        show_command_error(shell, "cd", strerror(errno), errno);
-}
-
 void update_pwd(t_shell *shell, char *name)
 {
-    char cwd[256];
+	char cwd[256];
 
-    ft_bzero(cwd, sizeof(cwd));
-    if (getcwd(cwd, sizeof(cwd)))
-        set_env(shell, ft_strdup(name), ft_strdup(cwd));
-    else
-        perror("Error: getcwd() failed");
+	ft_bzero(cwd, sizeof(cwd));
+	if (getcwd(cwd, sizeof(cwd)))
+	{
+		if (get_env(shell, name))
+			set_env(shell, ft_strdup(name), ft_strdup(cwd));
+		else
+			add_env(shell, ft_strdup(name), ft_strdup(cwd));
+	}
+	else
+		perror("Error: getcwd() failed");
+}
+
+char *fix_home_path(t_shell *shell, t_cmd *cmd)
+{
+	char *path;
+	char *home;
+
+	home = get_env(shell, "HOME");
+	if (!home)
+		return (0);
+	if (cmd->args[1])
+	{
+		if (ft_strncmp(cmd->args[1], "~/", 2) == 0)
+			path = ft_strjoin(ft_strdup(home), cmd->args[1] + 2);
+		else if (str_compare(cmd->args[1], "~") == 0)
+			path = ft_strdup(home);
+		else
+			path = ft_strdup(cmd->args[1]);
+	}
+	else
+		path = ft_strdup(home);
+	return (path);
 }
 
 int exec_cd(t_shell *shell, t_cmd *cmd)
 {
-    int i;
-    char **dirs;
+	char *path;
+	char *oldpwd;
+	char *tmp;
 
-    update_pwd(shell, "OLDPWD");
-    if (!cmd->args[1] || (cmd->args[1] && (str_compare(cmd->args[1], "~") == 0 || str_compare(cmd->args[1], "~/") == 0)))
-        change_dir(shell, get_env(shell, "HOME"));
-    else if (cmd->args[1] && str_compare(cmd->args[1], "-") == 0)
-        ft_putendl_fd(get_env(shell, "PWD"), 1);
-    else if (ft_strchr(cmd->args[1], '/') != 0)
-    {
-        i = -1;
-        dirs = ft_split(cmd->args[1], '/');
-        while (dirs[++i])
-            change_dir(shell, dirs[i]);
-        free_array(dirs);
-    }
-    else
-        change_dir(shell, cmd->args[1]);
-    update_pwd(shell, "PWD");
-    return (0);
+	path = fix_home_path(shell, cmd);
+	if (!path)
+		return (0);
+	else if (str_compare(path, "-") == 0)
+	{
+		oldpwd = get_env(shell, "OLDPWD");
+		tmp = path;
+		if (!oldpwd)
+			path = ft_strdup(get_env(shell, "PWD"));
+		else
+			path = ft_strdup(oldpwd);
+		free(tmp);
+	}
+	update_pwd(shell, "OLDPWD");
+	if (chdir(path) != 0)
+		show_command_error(shell, cmd->name, strerror(errno), errno);
+	update_pwd(shell, "PWD");
+	free(path);
+	return (0);
 }
-
-// bash: cd: fff: No such file or directory
-/*
-cd (change to home)
-cd .
-cd ..
-cd $HOME
-cd fakepath
-cd tmp/gnl
-cd //
- */
