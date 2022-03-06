6bdaeb27869e28c9a8918f0b59262189ed693cd5
ricardoreves <ricardo.reves@gmail.com>
2022-03-06 04:56:36 +0100

    lint"norminette

--- incs/minishell.h
+++ incs/minishell.h
@@ -6,37 +6,35 @@
 /*   By: rpinto-r <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 17:57:18 by rpinto-r          #+#    #+#             */
-/*   Updated: 2022/03/05 21:59:42 by rpinto-r         ###   ########.fr       */
+/*   Updated: 2022/03/06 04:56:07 by rpinto-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
-#define MINISHELL_H
+# define MINISHELL_H
 
-#define VERSION "0.1.1"
-#define NAME "\e[1;33mminishell\e[0m:"
-#define DOLLAR "\e[1;37m$ \e[0m"
-#define READ_END 0
-#define WRITE_END 1
-#define CONFIGFILE ".config/parser/bash.conf"
-#define LOGFILE "minishell.log"
+# define VERSION "0.1.1"
+# define NAME "\e[1;33mminishell\e[0m:"
+# define DOLLAR "\e[1;37m$ \e[0m"
+# define CONFIGFILE ".config/parser/bash.conf"
+# define LOGFILE "minishell.log"
 
-//#include <libft.h>
-//#include <libparser.h>
-#include "../libft/libft.h"
-#include "../parser/inc/libparser.h"
-#include "../printf/inc/ft_printf.h"
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <sys/wait.h>
-#include <signal.h>
+//# include <libft.h>
+//# include <libparser.h>
+# include "../libft/libft.h"
+# include "../parser/inc/libparser.h"
+# include "../printf/inc/ft_printf.h"
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <errno.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <signal.h>
 
 typedef enum e_bash_token_id
 {
@@ -62,109 +60,109 @@ typedef enum e_bash_token_id
 	id_builtin_export,
 	id_builtin_pwd,
 	id_builtin_unset
-} t_bash_token_id;
+}	t_bash_token_id;
 
 typedef struct s_cmd
 {
-	char *name;
-	char **args;
-	char *redirect_path;
-	t_bash_token_id redirect_id;
-	struct s_cmd *next;
-} t_cmd;
+	char			*name;
+	char			**args;
+	char			*redirect_path;
+	t_bash_token_id	redirect_id;
+	struct s_cmd	*next;
+}	t_cmd;
 
 typedef struct s_shell
 {
-	char *cmdline;
-	char *config;
-	t_cmd *cmds;
-	int **pipes;
-	int *pids;
-	char **envs;
-	int num_cmds;
-	int num_pipes;
-	int exit_status;
-} t_shell;
+	t_cmd	*cmds;
+	char	*cmdline;
+	char	*config;
+	char	**envs;
+	int		**pipes;
+	int		*pids;
+	int		num_cmds;
+	int		num_pipes;
+	int		exit_status;
+}	t_shell;
 
-void print_array(char *arr[]);
-void free_array(char *arr[]);
-int array_length(char *arr[]);
-char **clone_array(char *arr[]);
-void push_array(char *str, char ***arr);
+void	print_array(char *arr[]);
+void	free_array(char *arr[]);
+int		array_length(char *arr[]);
+char	**clone_array(char *arr[]);
+void	push_array(char *str, char ***arr);
 
-char *get_evaluate_value(t_shell *shell, char *str, int *i);
-char *evaluate_str_env(t_shell *shell, char *str, int k);
+char	*get_evaluate_value(t_shell *shell, char *str, int *i);
+char	*evaluate_str_env(t_shell *shell, char *str, int k);
 
-void free_shell(t_shell *shell);
-void free_pipes(int **pipes, int size);
+void	free_shell(t_shell *shell);
+void	free_pipes(int **pipes, int size);
 
-void close_pipes(t_shell *shell);
-int create_pipes(t_shell *shell);
+void	close_pipes(t_shell *shell);
+int		create_pipes(t_shell *shell);
 
-int contain_env(char *str, char *name);
-int add_env(t_shell *shell, char *name, char *value);
-char *get_env(t_shell *shell, char *name);
-int set_env(t_shell *shell, char *name, char *value);
-int unset_env(t_shell *shell, char *name);
-int init_envs(t_shell *shell, char *envp[]);
+int		contain_env(char *str, char *name);
+int		add_env(t_shell *shell, char *name, char *value);
+char	*get_env(t_shell *shell, char *name);
+int		set_env(t_shell *shell, char *name, char *value);
+int		unset_env(t_shell *shell, char *name);
+int		init_envs(t_shell *shell, char *envp[]);
 
-void handle_signals(int signum);
-void override_signals(void);
+void	handle_signals(int signum);
+void	override_signals(void);
 
-char *str_joins(char *str1, char *str2, char *sep);
-char *str_duplicate_nbytes(char *str, size_t n);
-int str_compare(char *str1, char *str2);
+char	*str_joins(char *str1, char *str2, char *sep);
+char	*str_duplicate_nbytes(char *str, size_t n);
+int		str_compare(char *str1, char *str2);
 
-int show_command_error(t_shell *shell, char *cmd, char *msg, int num);
-void save_exit_status(t_shell *shell);
+int		show_command_error(t_shell *shell, char *cmd, char *msg, int num);
+void	save_exit_status(t_shell *shell);
 
-int exec_cd(t_shell *shell, t_cmd *cmd);
-int exec_echo(t_shell *shell, t_cmd *cmd);
-int exec_env(t_shell *shell);
-int exec_exit(t_shell *shell);
-int exec_export(t_shell *shell, t_cmd *cmd);
-int exec_pwd(t_shell *shell);
-int exec_unset(t_shell *shell, t_cmd *cmd);
+int		exec_cd(t_shell *shell, t_cmd *cmd);
+int		exec_echo(t_shell *shell, t_cmd *cmd);
+int		exec_env(t_shell *shell);
+int		exec_exit(t_shell *shell);
+int		exec_export(t_shell *shell, t_cmd *cmd);
+int		exec_pwd(t_shell *shell);
+int		exec_unset(t_shell *shell, t_cmd *cmd);
 
-void init_prompt(t_shell *shell);
-void init_asciiart(void);
-int is_builtin_command(char *name);
-void exec_builtin_command(t_shell *shell, t_cmd *cmd);
-int exec_single_command(t_shell *shell, t_cmd *cmd);
+void	init_prompt(t_shell *shell);
+void	init_asciiart(void);
+int		is_builtin_command(char *name);
+void	exec_builtin_command(t_shell *shell, t_cmd *cmd);
+int		exec_single_command(t_shell *shell, t_cmd *cmd);
 
-int is_command_not_found(char *path, char **name);
-int is_directory(char *path);
-int is_file_permission_denied(char *name);
-int is_file_not_found(char *name);
+int		is_command_not_found(char *path, char **name);
+int		is_directory(char *path);
+int		is_file_permission_denied(char *name);
+int		is_file_not_found(char *name);
 
-void handle_commands(t_shell *shell);
-void process_command(t_shell *shell, t_cmd *cmd, int num);
-void wait_pids(t_shell *shell);
-void create_pids(t_shell *shell);
-int is_directory(char *path);
+void	handle_commands(t_shell *shell);
+void	process_command(t_shell *shell, t_cmd *cmd, int num);
+void	wait_pids(t_shell *shell);
+void	create_pids(t_shell *shell);
+int		is_directory(char *path);
 
-int redirect_input(t_cmd *cmd);
-int redirect_output(t_cmd *cmd);
-void handle_redirect(t_cmd *cmd);
+int		redirect_input(t_cmd *cmd);
+int		redirect_output(t_cmd *cmd);
+void	handle_redirect(t_cmd *cmd);
 
-int parse_command_line(t_shell *shell);
-void sanitize_quotes_token(t_token *token);
-void evaluate_str_env_token(t_token *token, t_shell *shell);
-void set_builtin_token_id(t_token *token);
-char **token_to_string(t_token *token_from, t_token *token_to);
-int is_builtin_token_id(int id);
-int is_redirect_token_id(int id);
-int is_word_token_id(int id);
+int		parse_command_line(t_shell *shell);
+void	sanitize_quotes_token(t_token *token);
+void	evaluate_str_env_token(t_token *token, t_shell *shell);
+void	set_builtin_token_id(t_token *token);
+char	**token_to_string(t_token *token_from, t_token *token_to);
+int		is_builtin_token_id(int id);
+int		is_redirect_token_id(int id);
+int		is_word_token_id(int id);
 
-void prepare_cmds(t_token *token, t_shell *shell);
-void print_cmds(t_cmd *cmds);
-void free_cmds(t_cmd *cmds);
-int count_cmds(t_cmd *cmds);
+void	prepare_cmds(t_token *token, t_shell *shell);
+void	print_cmds(t_cmd *cmds);
+void	free_cmds(t_cmd *cmds);
+int		count_cmds(t_cmd *cmds);
 
 // Log
-int ft_gettime(void);
-int log_open(void);
-void log_message(char *msg);
-void log_token(t_token *token);
-void log_cmds(t_cmd *cmds);
+int		ft_gettime(void);
+int		log_open(void);
+void	log_message(char *msg);
+void	log_token(t_token *token);
+void	log_cmds(t_cmd *cmds);
 #endif
