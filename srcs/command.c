e05a54d0cec375f7ec77b0d4293a8035b728a880
ricardoreves <ricardo.reves@gmail.com>
2022-03-06 18:07:35 +0100

    refactor: all functions

--- srcs/command.c
+++ srcs/command.c
@@ -6,16 +6,16 @@
 /*   By: rpinto-r <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 18:35:14 by rpinto-r          #+#    #+#             */
-/*   Updated: 2022/03/05 21:59:42 by rpinto-r         ###   ########.fr       */
+/*   Updated: 2022/03/06 18:04:08 by rpinto-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void handle_commands(t_shell *shell)
+void	handle_commands(t_shell *shell)
 {
-	int i;
-	t_cmd *cmd;
+	int		i;
+	t_cmd	*cmd;
 
 	i = 0;
 	cmd = shell->cmds;
@@ -37,29 +37,34 @@ void handle_commands(t_shell *shell)
 	save_exit_status(shell);
 }
 
-int exec_single_command(t_shell *shell, t_cmd *cmd)
+int	is_invalid_command(t_shell *shell, t_cmd *cmd)
 {
-	pid_t pid;
-	int status;
-
-	if (is_builtin_command(cmd->name))
-		exec_builtin_command(shell, cmd);
-	else if (is_directory(cmd->name))
-		show_command_error(shell, cmd->name, "is a directory", 126);
+	if (is_directory(cmd->name))
+		show_command_error(shell, cmd->name, MSG_IS_DIRECTORY, 126);
 	else if (is_file_not_found(cmd->name))
-		show_command_error(shell, cmd->name, "not such file or directory", 127);
+		show_command_error(shell, cmd->name, MSG_FILE_NOT_FOUND, 127);
 	else if (is_command_not_found(get_env(shell, "PATH"), &cmd->name))
-		show_command_error(shell, cmd->name, "command not found", 127);
+		show_command_error(shell, cmd->name, MSG_COMMAND_NOT_FOUND, 127);
 	else if (is_file_permission_denied(cmd->name))
-		show_command_error(shell, cmd->name, "permission denied", 126);
-	else
+		show_command_error(shell, cmd->name, MSG_PERMISSION_DENIED, 126);
+	return (shell->exit_status);
+}
+
+int	exec_single_command(t_shell *shell, t_cmd *cmd)
+{
+	pid_t	pid;
+	int		status;
+
+	if (is_builtin_command(cmd))
+		exec_builtin_command(shell, cmd);
+	else if (is_invalid_command(shell, cmd) == 0)
 	{
 		pid = fork();
 		if (pid == -1)
 			perror("Error: fork() failed\n");
 		else if (pid == 0)
 		{
-			handle_redirect(shell->cmds);
+			handle_redirect_file(shell->cmds);
 			if (execve(shell->cmds->name, shell->cmds->args, shell->envs))
 				show_command_error(shell, cmd->name, strerror(errno), errno);
 			exit(shell->exit_status);
@@ -71,47 +76,23 @@ int exec_single_command(t_shell *shell, t_cmd *cmd)
 	return (0);
 }
 
-void process_command(t_shell *shell, t_cmd *cmd, int num)
+void	process_command(t_shell *shell, t_cmd *cmd, int num)
 {
 	shell->pids[num] = fork();
 	if (shell->pids[num] < 0)
 		perror("Error: fork() failed");
-	else if (shell->pids[num] == 0) // Child
+	else if (shell->pids[num] == 0)
 	{
-		// input
-		if (num > 0)
-		{
-			// printf("input -> %s pipe: [%d][0] std: 0\n", cmd->name, num - 1);
-			dup2(shell->pipes[num - 1][0], 0);
-		}
-		else if (num == 0) // first command
-		{
-			handle_redirect(cmd);
-		}
-
-		// output
-		if (num < shell->num_cmds - 1)
-		{
-			// printf("output -> %s pipe: [%d][1] std: 1\n", cmd->name, num);
-			dup2(shell->pipes[num][1], 1);
-		}
-		else if (num == shell->num_cmds - 1) // last command
-		{
-			handle_redirect(cmd);
-		}
+		redirect_input(shell, cmd, num);
+		redirect_output(shell, cmd, num);
 		close_pipes(shell);
-		if (is_builtin_command(cmd->name))
+		if (is_builtin_command(cmd))
 			exec_builtin_command(shell, cmd);
-		else if (is_directory(cmd->name))
-			show_command_error(shell, cmd->name, "is a directory", 126);
-		else if (is_file_not_found(cmd->name))
-			show_command_error(shell, cmd->name, "not such file or directory", 127);
-		else if (is_command_not_found(get_env(shell, "PATH"), &cmd->name))
-			show_command_error(shell, cmd->name, "command not found", 127);
-		else if (is_file_permission_denied(cmd->name))
-			show_command_error(shell, cmd->name, "permission denied", 126);
-		else if (execve(cmd->name, cmd->args, shell->envs))
-			show_command_error(shell, cmd->name, strerror(errno), errno);
+		else if (is_invalid_command(shell, cmd) == 0)
+		{
+			if (execve(cmd->name, cmd->args, shell->envs))
+				show_command_error(shell, cmd->name, strerror(errno), errno);
+		}
 		exit(shell->exit_status);
 	}
 }
