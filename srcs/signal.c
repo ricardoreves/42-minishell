0dca88fcd63ae4878562296a1313fbb1853dc37e
ricardoreves <ricardo.reves@gmail.com>
2022-03-07 15:53:20 +0100

    update: comment debug code

--- srcs/signal.c
+++ srcs/signal.c
@@ -6,7 +6,7 @@
 /*   By: rpinto-r <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/05 14:58:20 by rpinto-r          #+#    #+#             */
-/*   Updated: 2022/03/07 00:40:13 by rpinto-r         ###   ########.fr       */
+/*   Updated: 2022/03/07 15:45:10 by rpinto-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,21 +22,20 @@ void handle_signals(int signum)
 		rl_redisplay();
 	}
 	//printf("signum: %d\n", signum);
-	if (signum == SIGSEGV)
-	{
-		kill(getpid(), SIGTERM);
-	}
+	//if (signum == SIGSEGV)
+	//{
+	//	kill(getpid(), SIGTERM);
+	//}
 }
 
 void init_signals(void)
 {
-
 	// signal(SIGTERM, handle_signals);
 	// signal(SIGSTOP, handle_signals);
 	// signal(SIGKILL, handle_signals);
-	signal(SIGSEGV, handle_signals);
+	// signal(SIGSEGV, handle_signals);
 	// signal(SIGABRT, handle_signals);
-	//signal(SIGQUIT, handle_signals);
+	signal(SIGQUIT, handle_signals);
 	signal(SIGINT, handle_signals);
 	//signal(SIGQUIT, SIG_IGN);
 }
