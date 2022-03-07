2267982bd840c0f81eb61630e771488aee968189
ricardoreves <ricardo.reves@gmail.com>
2022-03-07 21:55:43 +0100

    fix: bug echo -n

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: rpinto-r <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 21:40:07 by rpinto-r          #+#    #+#             */
-/*   Updated: 2022/03/06 04:30:46 by rpinto-r         ###   ########.fr       */
+/*   Updated: 2022/03/07 21:54:47 by rpinto-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,8 @@ int	exec_echo(t_shell *shell, t_cmd *cmd)
 	size = array_length(cmd->args);
 	if (size > 1)
 	{
+		if (str_compare(cmd->args[1], "-n") == 0 && size == 2)
+			return (0);
 		while (cmd->args && cmd->args[i])
 		{
 			ft_putstr_fd(cmd->args[i], 1);
