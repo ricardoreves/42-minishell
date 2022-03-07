e1a78d21a58add0a8dbfd948635f6855e6f6e4e0
ricardoreves <ricardo.reves@gmail.com>
2022-03-07 01:10:41 +0100

    debug: signals

--- srcs/signal.c
+++ srcs/signal.c
@@ -6,25 +6,37 @@
 /*   By: rpinto-r <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/05 14:58:20 by rpinto-r          #+#    #+#             */
-/*   Updated: 2022/03/06 15:14:28 by rpinto-r         ###   ########.fr       */
+/*   Updated: 2022/03/07 00:40:13 by rpinto-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	handle_signals(int signum)
+void handle_signals(int signum)
 {
 	if (signum == SIGINT)
 	{
-		printf("\n");
+		ft_putendl_fd("", 1);
 		rl_on_new_line();
 		rl_replace_line("", 1);
 		rl_redisplay();
 	}
+	//printf("signum: %d\n", signum);
+	if (signum == SIGSEGV)
+	{
+		kill(getpid(), SIGTERM);
+	}
 }
 
-void	init_signals(void)
+void init_signals(void)
 {
+
+	// signal(SIGTERM, handle_signals);
+	// signal(SIGSTOP, handle_signals);
+	// signal(SIGKILL, handle_signals);
+	signal(SIGSEGV, handle_signals);
+	// signal(SIGABRT, handle_signals);
+	//signal(SIGQUIT, handle_signals);
 	signal(SIGINT, handle_signals);
-	signal(SIGQUIT, SIG_IGN);
+	//signal(SIGQUIT, SIG_IGN);
 }
