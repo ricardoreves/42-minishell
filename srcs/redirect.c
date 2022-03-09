d0df294679d19269b969afc989be5e85582afb8c
Daniel Thalmann <dthalman@c1r5s8.42lausanne.ch>
2022-03-09 16:56:25 +0100

    heredoc

--- srcs/redirect.c
+++ srcs/redirect.c
@@ -3,21 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   redirect.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rpinto-r <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*   By: dthalman <daniel@thalmann.li>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/27 21:00:04 by rpinto-r          #+#    #+#             */
-/*   Updated: 2022/03/06 17:37:47 by rpinto-r         ###   ########.fr       */
+/*   Updated: 2022/03/09 16:56:24 by dthalman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	handle_redirect_file(t_cmd *cmd)
+void	handle_redirect_file(t_shell *shell, t_cmd *cmd)
 {
 	if (cmd->redirect_id == id_in_std || cmd->redirect_id == id_in_file)
 		redirect_input_file(cmd);
 	if (cmd->redirect_id == id_out_write || cmd->redirect_id == id_out_append)
-		redirect_output_file(cmd);
+		redirect_output_file(shell, cmd);
 }
 
 void	redirect_input_file(t_cmd *cmd)
@@ -40,12 +40,13 @@ void	redirect_input_file(t_cmd *cmd)
 	}
 }
 
-void	redirect_output_file(t_cmd *cmd)
+void	redirect_output_file(t_shell *shell, t_cmd *cmd)
 {
 	int	fd;
 
 	if (cmd->redirect_id == id_out_write)
-		fd = open(cmd->redirect_path, O_WRONLY | O_CREAT | O_TRUNC, S_IRWXU);
+		fd = here_doc(cmd->redirect_path, shell);
+		//fd = open(cmd->redirect_path, O_WRONLY | O_CREAT | O_TRUNC, S_IRWXU);
 	if (cmd->redirect_id == id_out_append)
 		fd = open(cmd->redirect_path, O_WRONLY | O_CREAT | O_APPEND, S_IRWXU);
 	if (fd == -1)
@@ -81,10 +82,38 @@ void	redirect_output(t_shell *shell, t_cmd *cmd, int num)
 		// printf("output -> %s pipe: [%d][1] std: 1\n", cmd->name, num);
 		if (cmd->redirect_id == id_out_write
 			|| cmd->redirect_id == id_out_append)
-			redirect_output_file(cmd);
+			redirect_output_file(shell, cmd);
 		else
 			dup2(shell->pipes[num][1], 1);
 	}
 	else if (num == shell->num_cmds - 1)
-		redirect_output_file(cmd);
+		redirect_output_file(shell, cmd);
+}
+
+int	here_doc(t_shell *shell, char *eof)
+{
+	char	*tmp_file;
+	char	*line;
+	int		fd;
+	int		mode;
+
+	tmp_file = str_joins(shell->workink_dir, ".minishell.tmp", "/");
+	mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+	fd = open(tmp_file, O_WRONLY | O_APPEND | O_CREAT, mode);
+	if (fd > -1)
+	{
+		line = get_nextline(0);
+		while (!ft_strncmp(eof, line, ft_strlen(eof)) &&
+			(ft_strlen(eof) + 1) == ft_strlen(line))
+		{
+			write(fd, line, ft_strlen(line));
+			free(line);
+			line = get_nextline(0);
+		}
+		free(line);
+	}
+	close(fd);
+	fd = open(tmp_file, O_RDONLY, mode);
+	free(tmp_file);
+	return (fd);
 }
