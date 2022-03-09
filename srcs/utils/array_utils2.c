72fbd5d26afa8dbff6cd3270c69365fa6efed897
Daniel Thalmann <dthalman@c1r5s8.42lausanne.ch>
2022-03-09 13:33:39 +0100

    array: add function to remove a element of list of string

--- srcs/utils/array_utils2.c
+++ srcs/utils/array_utils2.c
@@ -6,7 +6,7 @@
 /*   By: dthalman <daniel@thalmann.li>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 07:52:03 by dthalman          #+#    #+#             */
-/*   Updated: 2022/03/09 08:13:29 by dthalman         ###   ########.fr       */
+/*   Updated: 2022/03/09 13:30:00 by dthalman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ void	insert_array(char **new_arr, char *after, char ***arr)
 	int		new_len;
 
 	new_len = array_length(new_arr) + array_length(*arr) + 1;
-	tmp = ft_calloc(sizeof(char **), new_len + 1);
+	tmp = ft_calloc(sizeof(char **), new_len);
 	next = *arr;
 	i = -1;
 	while (*next)
@@ -47,4 +47,38 @@ void	insert_array(char **new_arr, char *after, char ***arr)
 	tmp[++i] = 0;
 	free_array(*arr);
 	(*arr) = tmp;
+}
+
+/**
+ * @brief supprime une chaine d'un tableau de chaine
+ * 
+ * @param to_remove 
+ * @param arr 
+ */
+void	remove_str_array(char *to_remove, char ***arr)
+{
+	int		i;
+	char	**tmp;
+	int		new_len;
+	int		found;
+
+	found = 0;
+	new_len = array_length(*arr);
+	tmp = ft_calloc(sizeof(char **), new_len + 1);
+	i = -1;
+	while (++i < new_len)
+	{
+		if ((*arr)[i] != (to_remove))
+			tmp[i - found] = (*arr)[i];
+		else
+			found = 1;
+	}
+	tmp[++i - found] = 0;
+	if (found)
+	{
+		free(*arr);
+		(*arr) = tmp;
+	}
+	else
+		free(tmp);
 }
