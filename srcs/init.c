e30820bd4094f6fb3a8c676ae56a53ed9694fb91
ricardoreves <ricardo.reves@gmail.com>
2022-03-12 01:45:28 +0100

    add: function free_shell

--- srcs/init.c
+++ srcs/init.c
@@ -6,7 +6,7 @@
 /*   By: rpinto-r <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 21:54:07 by dthalman          #+#    #+#             */
-/*   Updated: 2022/03/10 15:30:56 by rpinto-r         ###   ########.fr       */
+/*   Updated: 2022/03/12 01:20:02 by rpinto-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,11 @@ void	init_asciiart(void)
 
 void	init_config(t_shell *shell)
 {
-	shell->workink_dir = malloc(256);
-	if (!shell->workink_dir)
+	shell->working_dir = malloc(256);
+	if (!shell->working_dir)
 		exec_exit(shell);
-	if (getcwd(shell->workink_dir, 256))
-		shell->config = str_joins(shell->workink_dir, CONFIGFILE, "/");
+	if (getcwd(shell->working_dir, 256))
+		shell->config = str_joins(shell->working_dir, CONFIGFILE, "/");
 	handle_echoctl();
 }
 
@@ -46,4 +46,21 @@ t_shell	*get_shell(void)
 	static t_shell	shell;
 
 	return (&shell);
+}
+
+void	free_shell(t_shell *shell)
+{
+	if (shell->cmds)
+		free_cmds(&(shell->cmds));
+	if (shell->envs)
+		free_array(shell->envs);
+	if (shell->config)
+		free(shell->config);
+	if (shell->working_dir)
+		free(shell->working_dir);
+	shell->cmds = 0;
+	shell->envs = 0;
+	shell->config = 0;
+	shell->working_dir = 0;
+	shell = 0;
 }
